17753d02f4a072c9257770e02e027ed6d4eac88d
liyajing <575567292@qq.com>
2019-08-03 19:15:33 +0800

    项目

---
+++ 897/src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vue);
+
+const state={}
+const action={}
+const mutations={}
+
+export default new Vuex.Store({
+    state,
+    action,
+    mutations,
+    modules: {}
+})
